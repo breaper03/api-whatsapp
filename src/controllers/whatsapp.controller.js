35a4641e08ef56c4d550649dac466f76fb9ce7d4
breaper03 <breaper2021@gmail.com>
2024-02-03 00:17:40 -0400

    Verify Token method

--- src/controllers/whatsapp.controller.js
+++ src/controllers/whatsapp.controller.js
@@ -1,5 +1,19 @@
 const verifyToken = (req, res) => {
-  return res.send("hola verify token")
+
+  try {
+    const accessToken = "LKJ124JH124J1K2";
+    const token = req.query["hub.verify_token"];
+    const challenge = req.body["hub.challenge"];
+    
+    token === accessToken && token !== null && challenge !== null
+      ? res.send(challenge)
+      : res.status(400).send()
+
+  } catch (error) {
+    res.status(400).send()
+  }
+
+  // return res.send("hola verify token")
 }
 
 const receivedMessage = (req, res) => {
